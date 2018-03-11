f1b656761826ebb51fb56c1715b58f01eb77a811
Antoine <antoine.dujardin@epita.fr>
2018-03-11 15:50:18 +0100

    simplifier code, ajouter commentaires

--- iut/s4_maths/src/Graphe.java
+++ iut/s4_maths/src/Graphe.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 
 class Graphe{
+    // Stocke les sommets
     private LinkedList<Sommet> sommets = new LinkedList<>();
     
     private int nb_sommets = 0;
@@ -13,45 +14,56 @@ class Graphe{
 
     }
 
+    // Ajoute un Sommet au graphe
     void ajouterSommet(){
         sommets.add(new Sommet(nb_sommets));
         nb_sommets++;
     }
+    // Supprime le sommet n. id du graphe
     void supprimerSommet(int id){
         sommets.remove(getSommet(id));
     }
 
-    void ajouterArete(int startId, int endId, int capacite, int flot) {
+    // Ajoute une arete au graphe.
+    void ajouterArete(int startId, int endId, int capacite, int flot) throws Exception {
         if (flot > capacite) {
-            System.out.println("Flot > capacite");
+            throw new Exception("Flot > capacite");
         }
         else {
             getSommet(startId).ajouterArete(new Arete(startId, endId, capacite, flot));
         }
     }
+    // Ajoute une arete au graphe.
     void ajouterArete(int startId, int endId, int capacite){
         getSommet(startId).ajouterArete(new Arete(startId, endId, capacite));
     }
+    // Ajoute une arete au graphe.
     void ajouterArete(int startId, int endId){
         getSommet(startId).ajouterArete(new Arete(startId, endId));
     }
+    // Ajoute une arete au graphe.
     void supprimerArete(int startId, int endId){
         getSommet(startId).supprimerArete(endId);
     }
 
+    // Choisir l'entree du graphe
     void setEntree(int id){
         entree_id = id;
     }
+    // Choisir la sortie du graphe
     void setSortie(int id){
         sortie_id = id;
     }
 
 
+    // Verifie si le flot est valide
     boolean flotValide(){
+        // Il faut une entree et une sortie pour avoir un flot valide
         if (entree_id == null || sortie_id == null) {
+            System.out.println("Entree ou sortie non definie");
             return false;
         }
-
+        // Verifie la somme de flot entrant et sortant pour tout les sommets
         for (Sommet currSommet : sommets) {
             LinkedList<Arete> incoming = getIncomingAretes(currSommet.getId());
             LinkedList<Arete> outcoming = currSommet.aretes;
@@ -60,27 +72,22 @@ class Graphe{
 
             currSommet.sumIn = incomingSum;
             if (incomingSum != outcomingSum) {
-                if (currSommet.getId() == entree_id) {
-                    if (incomingSum > 0) {
-                        System.out.println("flow entering entry");
-                        return false;
-                    }
+                if (currSommet.getId() == entree_id && incomingSum > 0) {
+                    System.out.println("flow entering entry");
                 }
-                else if (currSommet.getId() == sortie_id) {
-                    if (outcomingSum > 0) {
-                        System.out.println("flow leaving exit");
-                        return false;
-                    }
+                else if (currSommet.getId() == sortie_id && outcomingSum > 0) {
+                    System.out.println("flow leaving exit");
                 }
                 else {
                     System.out.println(String.format("Sommet %d: incoming %d != outgoing %d", currSommet.getId(), incomingSum, outcomingSum));
-                    return false;
                 }
+                return false;
             }
         }
         return true;
     }
 
+    // Commence le marquage
     void marquer(){
         if (!flotValide()) {
             System.out.println("Flot invalide");
@@ -91,24 +98,28 @@ class Graphe{
         }
 
         LinkedList<Sommet> file = new LinkedList<>();
-        marquerIteratif(file);
-    }
-    private void marquerIteratif(LinkedList<Sommet> file){
         Sommet entree = getSommet(entree_id);
 
         for (Arete outcomingArete : entree.aretes) {
             file.add(getSommet(outcomingArete.getArrivee()));
         }
 
+        marquerIteratif(file);
+    }
+    // Effectue le marquage interativement avec les sommets contenus dans file
+    private void marquerIteratif(LinkedList<Sommet> file){
         while (!file.isEmpty()) {
+            //Prendre un sommet dans la file
             Sommet currSommet = file.pop();
 
+            // Marquer le sommet
             currSommet.marquagePlus = new LinkedList<>();
             currSommet.marquageMoins = new LinkedList<>();
 
             marquerSommetPlus(currSommet);
             //marquerSommetMoins(currSommet);
 
+            // S'il a ete marquer, ajouter ses fils dans la file
             if (!currSommet.marquagePlus.isEmpty() || !currSommet.marquageMoins.isEmpty()) {
                 for (Arete outcomingArete : currSommet.aretes) {
                     file.add(getSommet(outcomingArete.getArrivee()));
@@ -116,6 +127,7 @@ class Graphe{
             }
         }
     }
+    // Marquage positif d'un sommet
     private void marquerSommetPlus(Sommet sommet){
         LinkedList<Arete> incoming = getIncomingAretes(sommet.getId());
         for (Arete incomingArete : incoming) {
@@ -125,6 +137,7 @@ class Graphe{
             }
         }
     }
+    // Marquage negatif d'un sommet
     private void marquerSommetMoins(Sommet sommet){
         LinkedList<Arete> outcoming = sommet.aretes;
         for (Arete outcomingArete : outcoming) {
@@ -134,7 +147,29 @@ class Graphe{
         }
     }
 
+    void augmenterFlot(){
 
+    }
+    // Donne le flot maximal entre les arretes
+    int maxFlot(Arete[] aretes) throws Exception{
+        int minFlot = Integer.MAX_VALUE;
+        int lastArrivee = aretes[0].getDepart();
+
+        for(Arete arete : aretes){
+            if (lastArrivee != arete.getDepart()){ // On doit partir du dernier sommet d'arrivee.
+                throw new Exception("Les Aretes ne sont pas liees!");
+            }
+            if (arete.getFlot() < minFlot){
+                minFlot = arete.getFlot();
+            }
+
+            lastArrivee = arete.getArrivee();
+        }
+        return minFlot;
+    }
+
+
+    // Obtenir le sommet avec id
     private Sommet getSommet(int id){
         for (Sommet currSommet : sommets) {
             if (currSommet.getId() == id) {
@@ -144,6 +179,7 @@ class Graphe{
         return null;
     }
 
+    // Obtenir les aretes allant dans id
     private LinkedList<Arete> getIncomingAretes(int id){
         LinkedList<Arete> results = new LinkedList<>();
         for (Sommet currSommet : sommets) {
@@ -156,6 +192,7 @@ class Graphe{
         return results;
     }
 
+    // Obtenir la somme des flots des arretes de la liste
     private int getArreteFlotSum(LinkedList<Arete> areteList){
         int sum = 0;
         for (Arete arete : areteList) {
@@ -163,6 +200,7 @@ class Graphe{
         }
         return sum;
     }
+    // Obtenir la somme des capacites des arretes de la liste
     private int getArreteCapaciteSum(LinkedList<Arete> areteList){
         int sum = 0;
         for (Arete arete : areteList) {
@@ -172,6 +210,7 @@ class Graphe{
     }
 
 
+    // Generer une representation en string du graphe
     public String toString(){
         StringBuilder resultBuilder = new StringBuilder();
         resultBuilder.append(String.format("Graphe: %d sommets\n", nb_sommets));
