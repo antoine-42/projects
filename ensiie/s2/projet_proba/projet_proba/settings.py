1e8f106f520f9df2290189159df61836f0c4809c
Antoine <antoinedujardin42@gmail.com>
2019-03-28 11:23:53 +0100

    gitignore, allow hosts

--- ensiie/s2/projet_proba/projet_proba/settings.py
+++ ensiie/s2/projet_proba/projet_proba/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from socket import gethostname, gethostbyname
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = '&pw=wpns42!yk1f+(*sy%#maurqylh9@$ot6ofa0job!2g7xx8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [gethostname(), gethostbyname(gethostname()),]
 
 
 # Application definition
